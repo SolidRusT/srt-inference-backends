cd86a4ee9239e35ed1c40fb6627ed7e5be425104
Shaun Prince <suparious@solidrust.net>
2024-04-01 23:48:00 -0700

    Update test-api-requests.py

--- tabby-api/test-api-requests.py
+++ tabby-api/test-api-requests.py
@@ -2,7 +2,7 @@ import requests
 from typing import List
 
 # Configuration parameters
-OPENAI_API_KEY = 'sk-OSxxxxx'
+OPENAI_API_KEY = 'sk-not-required'
 MODEL = 'not-required'
 ENDPOINT_URL = 'http://hades:8091/v1/completions'  # Endpoint URL
 HEADERS = {"Authorization": f"Bearer {OPENAI_API_KEY}"}
