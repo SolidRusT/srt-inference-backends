98f83bce3a0f9f82436d62c2fcfabe00342f34c2
Shaun Prince <suparious@solidrust.net>
2024-04-02 18:27:31 -0700

    Update launch-nvidia.sh to us consisten model foler structure

--- tabby-api/launch-nvidia.sh
+++ tabby-api/launch-nvidia.sh
@@ -3,7 +3,7 @@ volume="${HOME}/hf_models"
 
 docker run --gpus all --shm-size 1g \
   -p 5000:5000 \
-  -v $volume:/data \
+  -v $volume:/hf_models \
   -v ./config.yml:/tabbyAPI/config.yml \
   -v ./api_tokens.yml:/tabbyAPI/api_tokens.yml \
   solidrust/tabby-api
