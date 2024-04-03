3dbc36a64576fcfb8b5288698950cc902e1dd0be
Shaun Prince <suparious@solidrust.net>
2024-04-02 18:11:24 -0700

    Update launch-nvidia.sh
    add configurable yml for nvidia tabby

--- tabby-api/launch-nvidia.sh
+++ tabby-api/launch-nvidia.sh
@@ -2,7 +2,9 @@
 
 volume="${HOME}/hf_models"
 
-docker run --gpus all --shm-size 1g -p 8091:8091 \
+docker run --gpus all --shm-size 1g \
+  -p 5000:5000 \
   -v $volume:/data \
+  -v ./config.yml:/tabbyAPI/config.yml \
+  -v ./api_tokens.yml:/tabbyAPI/api_tokens.yml \
   solidrust/tabby-api
-
