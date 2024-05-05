927f5bde600fb30c3b283848333bb9b34119ccbd
Shaun Prince <suparious@solidrust.net>
2024-05-04 21:54:51 -0700

    Create test-curl.sh

---
+++ ollama/test-curl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+curl http://hades:8081/v1/chat/completions \
+    -H "Content-Type: application/json" \
+    -d '{
+        "model": "llama3",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful AI assistant who answers mundane questions from humans."
+            },
+            {
+                "role": "user",
+                "content": "Why is the sky blue?"
+            }
+        ]
+    }'
